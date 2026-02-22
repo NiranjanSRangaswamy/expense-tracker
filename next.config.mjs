11dfec256f8b9bd3806c17f5824a76f89aac563f
Niranjan S.R <108211452+NiranjanSRangaswamy@users.noreply.github.com>
2026-02-22 16:25:38 +0530

    Add ESLint configuration to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
         allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+          eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
